b5c6a9ffb43c0efe4f733586224376017f9eff10
shelly schwartz <31975752+shellyganga@users.noreply.github.com>
2024-10-29 18:29:54 -0700

    Add files via upload

---
+++ package_folder/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Shelly S',
+    author_email='blahblah@blah',
+    description='just saying hello',
+)
---
+++ package_folder/testing.py
@@ -0,0 +1,3 @@
+from package.module import myfunc
+
+myfunc()
